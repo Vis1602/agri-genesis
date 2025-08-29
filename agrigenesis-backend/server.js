f081d49cf7df55c5024df2c619a4deb1d22c1338
Vis1602 <lokwanivishal456@gmail.com>
2025-08-30 01:41:14 +0530

    resolve cors error-1

--- agrigenesis-backend/server.js
+++ agrigenesis-backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "https://agri-genesis.vercel.app/"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "https://agri-genesis.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -79,7 +79,7 @@ io.on("connection", (socket) => {
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "https://agri-genesis.vercel.app/"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "https://agri-genesis.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
