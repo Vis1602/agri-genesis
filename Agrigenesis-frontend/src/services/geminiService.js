9c05c74262bb288225bc72c33a2dbac0071ff31b
Vis1602 <lokwanivishal456@gmail.com>
2026-01-26 11:23:28 +0530

    replaced gemini model from 2.0 to 3.0

--- Agrigenesis-frontend/src/services/geminiService.js
+++ Agrigenesis-frontend/src/services/geminiService.js
@@ -1,6 +1,6 @@
 const GEMINI_API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
 
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${GEMINI_API_KEY}`;
 
 
 
